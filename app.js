842a675a41a0bca03527c3410c076c6feb2d935d
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-10-06 21:48:28 -0600

    Enabled logging and changed map tiling and added calgary tower pin

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require("express");
 var path = require("path");
+var morgan = require("morgan");
 
 var app = express();
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms"),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
